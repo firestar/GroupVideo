ce2611446211e4a6f1ee409e5ce60597ce672b72
Nathaniel Davidson <nathaniel.davidson@gmail.com>
2013-09-22 21:11:12 -0700

    Added Escaping HTML
    html escaping

--- com/synload/groupvideo/request/RequestGenerator.java
+++ com/synload/groupvideo/request/RequestGenerator.java
@@ -26,7 +26,7 @@ public class RequestGenerator{
         JSONObject retJSON = new JSONObject();
         try {
             retJSON.put("action", "new_user");
-            retJSON.put("name", name);
+            retJSON.put("name", escapeHTML(name));
         } catch (JSONException e) {
             e.printStackTrace();
         }
@@ -36,7 +36,7 @@ public class RequestGenerator{
         JSONObject retJSON = new JSONObject();
         try {
             retJSON.put("action", "user_left");
-            retJSON.put("name", user);
+            retJSON.put("name", escapeHTML(user));
         } catch (JSONException e) {
             e.printStackTrace();
         }
@@ -82,7 +82,7 @@ public class RequestGenerator{
         JSONObject retJSON = new JSONObject();
         try {
             retJSON.put("action", "new_owner");
-            retJSON.put("name", owner);
+            retJSON.put("name", escapeHTML(owner));
         } catch (JSONException e) {
             e.printStackTrace();
         }
@@ -110,8 +110,8 @@ public class RequestGenerator{
         JSONObject retJSON = new JSONObject();
         try {
             retJSON.put("action", "load");
-            retJSON.put("movie_id", movie_id);
-            retJSON.put("title", title);
+            retJSON.put("movie_url", movie_id);
+            retJSON.put("title", escapeHTML(title));
         } catch (JSONException e) {
             e.printStackTrace();
         }
@@ -121,8 +121,8 @@ public class RequestGenerator{
         JSONObject retJSON = new JSONObject();
         try {
             retJSON.put("action", "message");
-            retJSON.put("name", name);
-            retJSON.put("text", msg);
+            retJSON.put("name", escapeHTML(name));
+            retJSON.put("text", escapeHTML(msg));
         } catch (JSONException e) {
             e.printStackTrace();
         }
@@ -132,7 +132,7 @@ public class RequestGenerator{
         JSONObject retJSON = new JSONObject();
         try {
             retJSON.put("action", "disconnect");
-            retJSON.put("reason", reason);
+            retJSON.put("reason", escapeHTML(reason));
         } catch (JSONException e) {
             e.printStackTrace();
         }
@@ -148,4 +148,147 @@ public class RequestGenerator{
         }
         return retJSON.toString();
     }
+    public String escapeHTML(String s) {
+        StringBuffer sb = new StringBuffer();
+        int n = s.length();
+        for (int i = 0; i < n; i++) {
+          char c = s.charAt(i);
+          switch (c) {
+          case '<':
+            sb.append("&lt;");
+            break;
+          case '>':
+            sb.append("&gt;");
+            break;
+          case '&':
+            sb.append("&amp;");
+            break;
+          case '"':
+            sb.append("&quot;");
+            break;
+          case 'à':
+            sb.append("&agrave;");
+            break;
+          case 'À':
+            sb.append("&Agrave;");
+            break;
+          case 'â':
+            sb.append("&acirc;");
+            break;
+          case 'Â':
+            sb.append("&Acirc;");
+            break;
+          case 'ä':
+            sb.append("&auml;");
+            break;
+          case 'Ä':
+            sb.append("&Auml;");
+            break;
+          case 'å':
+            sb.append("&aring;");
+            break;
+          case 'Å':
+            sb.append("&Aring;");
+            break;
+          case 'æ':
+            sb.append("&aelig;");
+            break;
+          case 'Æ':
+            sb.append("&AElig;");
+            break;
+          case 'ç':
+            sb.append("&ccedil;");
+            break;
+          case 'Ç':
+            sb.append("&Ccedil;");
+            break;
+          case 'é':
+            sb.append("&eacute;");
+            break;
+          case 'É':
+            sb.append("&Eacute;");
+            break;
+          case 'è':
+            sb.append("&egrave;");
+            break;
+          case 'È':
+            sb.append("&Egrave;");
+            break;
+          case 'ê':
+            sb.append("&ecirc;");
+            break;
+          case 'Ê':
+            sb.append("&Ecirc;");
+            break;
+          case 'ë':
+            sb.append("&euml;");
+            break;
+          case 'Ë':
+            sb.append("&Euml;");
+            break;
+          case 'ï':
+            sb.append("&iuml;");
+            break;
+          case 'Ï':
+            sb.append("&Iuml;");
+            break;
+          case 'ô':
+            sb.append("&ocirc;");
+            break;
+          case 'Ô':
+            sb.append("&Ocirc;");
+            break;
+          case 'ö':
+            sb.append("&ouml;");
+            break;
+          case 'Ö':
+            sb.append("&Ouml;");
+            break;
+          case 'ø':
+            sb.append("&oslash;");
+            break;
+          case 'Ø':
+            sb.append("&Oslash;");
+            break;
+          case 'ß':
+            sb.append("&szlig;");
+            break;
+          case 'ù':
+            sb.append("&ugrave;");
+            break;
+          case 'Ù':
+            sb.append("&Ugrave;");
+            break;
+          case 'û':
+            sb.append("&ucirc;");
+            break;
+          case 'Û':
+            sb.append("&Ucirc;");
+            break;
+          case 'ü':
+            sb.append("&uuml;");
+            break;
+          case 'Ü':
+            sb.append("&Uuml;");
+            break;
+          case '®':
+            sb.append("&reg;");
+            break;
+          case '©':
+            sb.append("&copy;");
+            break;
+          case '€':
+            sb.append("&euro;");
+            break;
+          case ' ':
+            sb.append("&nbsp;");
+            break;
+
+          default:
+            sb.append(c);
+            break;
+          }
+        }
+        return sb.toString();
+      }
 }
